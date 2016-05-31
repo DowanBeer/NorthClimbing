4f4f92ed60347a156617728be6e9d7187702f774
Plinz <antoine.duquennoy59@gmail.com>
2016-05-31 18:13:45 +0200

    I forget this one'

--- src/main/java/com/dowanBeer/articleRetreiver/rest/ArticleController.java
+++ src/main/java/com/dowanBeer/articleRetreiver/rest/ArticleController.java
@@ -9,7 +9,6 @@ import com.dowanBeer.articleRetreiver.beans.Article;
 @RestController
 public class ArticleController {
 
-
 	//https://spring.io/guides/gs/rest-service/ Tutorial
     @RequestMapping("/article")
     public Article getArticle(@RequestParam(value="oid", defaultValue="null") int name) {
